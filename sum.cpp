31a606ecb7103d111807b8b861b5378fe9739cbc
jayesh-bakle <jayeshbakleabd@gmail.com>
2022-12-29 18:26:59 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Sum(int a,int b)
+{
+    int ans = a + b;
+    return ans;
+}
+
+int main()
+{
+
+    cout<<"The sum is "<<Sum(12,65);
+
+
+
+    return 0;
+}
