72cbf3026115d6f92fa5590a3a8d1b1f8e2e3c7d
jayesh-bakle <jayeshbakleabd@gmail.com>
2022-12-29 19:12:04 +0530

    added the implementation for the diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Diff(int a,int b)
+{
+    int ans = a - b;
+    return ans;
+}
+
+int main()
+{
+
+    cout<<"The difference is "<<Diff(12,65);
+
+
+
+    return 0;
+}
