c549acacc7abcb1c3a57e16a2aa907e43e5646cc
jayesh-bakle <jayeshbakleabd@gmail.com>
2022-12-29 18:55:48 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Sum(int a,int b)
+{
+    int ans = a * b;
+    return ans;
+}
+
+int main()
+{
+
+    cout<<"The product is "<<Sum(12,65);
+
+
+
+    return 0;
+}
